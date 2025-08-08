46c6f7d0fa9534ada641901153f9429882011aec
Jayashree6322 <jayashree06032002@gmail.com>
2025-08-08 12:32:59 +0530

    Add files via upload
    Implemented function to show available library books.

--- library.cjs
+++ library.cjs
@@ -13,8 +13,20 @@ const availableBooks = () => books.filter(book => book.available);
 const checkBookLimit = users.map((user) => 
   user.borrowedBooks.length >= maxBorrowLimit ? "Limit reached" : "Can borrow more books");
 
+const dueDate = borrowDurationDays * 24 * 60 * 60 * 1000;
+
+const checkDueDate = () => {
+  const currentDate = new Date().toLocaleDateString();
+  const returnDate = new Date(Date.now() + dueDate).toLocaleDateString();
+return { 
+  CurrentDate: currentDate,
+  ReturnDate: returnDate 
+}};
+
 console.log(checkBookLimit);
+console.log(checkDueDate());
 console.log(availableBooks());
+
 
 
 
